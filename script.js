5b2226ea1c29172b1dfcdca0509b19a742105870
vvvvvlsm <131651173+vvvvvlsm@users.noreply.github.com>
2023-05-20 12:05:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-// 在页面加载完成后执行的函数
-window.onload = function() {
-    // 获取页面元素
-    var title = document.querySelector('h1');
+// 菜单按钮
+var menuTrigger = document.getElementById('menu-trigger');
+// 菜单内容
+var menuContent = document.getElementById('menu-content');
 
-    // 修改元素的内容
-    title.textContent = '欢迎访问我的个人主页';
+// 打开/关闭菜单
+function toggleMenu() {
+    menuContent.classList.toggle('open');
+}
 
-    // 添加事件监听器
-    title.addEventListener('click', function() {
-        alert('你点击了标题');
+// 点击菜单按钮打开/关闭菜单
+menuTrigger.addEventListener('click', toggleMenu);
+
+// 点击菜单以外的区域关闭菜单
+document.addEventListener('click', function(event) {
+    var target = event.target;
+    if (!menuContent.contains(target) && !menuTrigger.contains(target)) {
+        menuContent.classList.remove('open');
+    }
+});
+
+// 导航链接点击事件
+var navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        var targetSectionId = link.getAttribute('href').slice(1);
+        var targetSection = document.getElementById(targetSectionId);
+        if (targetSection) {
+            // 滚动到目标区域
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+            // 关闭菜单
+            menuContent.classList.remove('open');
+        }
     });
-};
+});
